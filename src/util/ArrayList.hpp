e654c3e7ffa2e651f30e30c675d7c2327257e007
moveaxebc <mzygdeaq@163.com>
2019-10-30 09:21:52 +0800

    a

--- src/util/ArrayList.hpp
+++ src/util/ArrayList.hpp
@@ -9,6 +9,9 @@ template<typename T>
 class ArrayList {
 public:
     explicit ArrayList(int initial_cap = 16);
+    ArrayList(const ArrayList& rhs);
+    ArrayList& operator=(const ArrayList& rhs);
+    T& operator[](int index);// Returns a Reference to the element at position n in the vector container.
     void add(T t);
     void insert(int index, T t);
     T get(int index);
