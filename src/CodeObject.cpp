bc775f05e3b6d8185c097c5b38c29dedd1ef58db
moveaxebc <mzygdeaq@163.com>
2019-10-30 17:59:12 +0800

    code object

--- src/CodeObject.cpp
+++ src/CodeObject.cpp
@@ -1,1 +1,26 @@
 #include "CodeObject.hpp"
+
+CodeObject::CodeObject(
+  int argCount,
+  int nlocals,
+  int stackSize,
+  int flag,
+  PyString *byteCodes,
+  ArrayList<PyObject*> *consts,
+  ArrayList<PyObject*> *names,
+  ArrayList<PyObject*> *varnames,
+  ArrayList<PyObject*> *freevars,
+  ArrayList<PyObject*> *cellvars,
+  PyString *fileName,
+  PyString *coName,
+  int lineno,
+  PyString *notable)
+  :
+  m_ArgCount(argCount),m_Nlocals(nlocals),m_StackSize(stackSize),m_Flag(flag),
+  m_ByteCodes(byteCodes),
+  m_Consts(consts),m_Names(names),m_Varnames(varnames),m_Freevars(freevars),m_Cellvars(cellvars),
+  m_FileName(fileName), m_CoName(coName),m_Lineno(lineno),m_Notable(notable)
+  NOEXCEPT
+{
+
+}
--- src/CodeObject.hpp
+++ src/CodeObject.hpp
@@ -2,13 +2,46 @@
 #define CODE_OBJECT_HPP
 
 #include "PyObject.hpp"
+#include "ArrayList.hpp"
+
+class PyString;
 
 class CodeObject : public PyObject {
 public:
-
+    explicit CodeObject(
+      int argCount,
+      int nlocals,
+      int stackSize,
+      int flag,
+      PyString *byteCodes,
+      ArrayList<PyObject*> *consts,
+      ArrayList<PyObject*> *names,
+      ArrayList<PyObject*> *varnames,
+      ArrayList<PyObject*> *freevars,
+      ArrayList<PyObject*> *cellvars,
+      PyString *fileName,
+      PyString *coName,
+      int lineno,
+      PyString *notable) NOEXCEPT;
+      ~CodeObject();
 
 private:
+    int m_ArgCount;                     /* 参数个数 */
+    int m_Nlocals;                      /* 局部变量个数 */
+    int m_StackSize;                    /* 当前段code使用的操作数栈的深度的最大值 */
+    int m_Flag;                         /* code的属性值 */
+    PyString *m_ByteCodes;              /* 字节码 */
 
+    ArrayList<PyObject*> *m_Consts;     /* 常量表 */
+    ArrayList<PyObject*> *m_Names;      /* 变量表 */
+    ArrayList<PyObject*> *m_Varnames;
+    ArrayList<PyObject*> *m_Freevars;
+    ArrayList<PyObject*> *m_Cellvars;
+
+    PyString *m_FileName;
+    PyString *m_CoName;
+    int m_Lineno;
+    PyString *m_Notable;
 };
 
 
