06fe97379fefbf40931563ea3edb9a2465b2cffb
moveaxebc <mzygdeaq@163.com>
2019-10-31 14:06:47 +0800

    std::make_shared

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,7 @@
 
 int main(int argc, char const *argv[]) {
   FileInputStream _stream("./test.txt");
-  // 1 ok
-  std::shared_ptr<BinaryFileParser> parser(new BinaryFileParser(&_stream));
+  auto parser = std::make_shared<BinaryFileParser>(&_stream);
   parser->parse();
-  // 2 error g++ -o app *.cpp -std=c++11
-  // auto parser = std::make_shared<BinaryFileParser>(new BinaryFileParser(&_stream));
-  // parser->parse();
   return 0;
 }
