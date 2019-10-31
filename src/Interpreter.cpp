6b30db99bcc7bb0f1b0fa846ddf4182c7df9483b
moveaxebc <mzygdeaq@163.com>
2019-10-31 15:57:13 +0800

    Mingw g++ didn't support std::mutex ???

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,6 +1,20 @@
 #include "Interpreter.hpp"
+#include <mutex>
+
+Interpreter* Interpreter::m_Instance = nullptr;
+
+Interpreter *Interpreter::get_instance()
+{
+    if (Interpreter::m_Instance == nullptr) {
+        std::lock_guard<std::mutex> lock(m_Mutex);
+        if(Interpreter::m_Instance == nullptr) {
+            Interpreter::m_Instance = new Interpreter();
+        }
+    }
+    return Interpreter::m_Instance;
+}
 
 void Interpreter::run(CodeObject *codes)
 {
-    
+
 }
--- src/Interpreter.hpp
+++ src/Interpreter.hpp
@@ -1,15 +1,37 @@
 #ifndef INTERPRETER_HPP
 #define INTERPRETER_HPP
 
+#include <mutex>
 #include "CodeObject.hpp"
+#include "NonCopyable.hpp"
 
-class Interpreter {
+class Interpreter : public fromboost::NonCopyable {
 
+  enum Status {
+      IS_OK,
+      IS_BREAK,
+      IS_CONTINUE,
+      IS_EXCEPTION,
+      IS_RETURN,
+      IS_YIELD,
+  };
+
+// public methods
 public:
+    static Interpreter *get_instance();
     void run(CodeObject* codes);
+
+// private methods
 private:
+    Interpreter() = default;
 
+// public fields
+public:
 
+// private fields
+private:
+    static Interpreter *m_Instance;
+    static std::mutex m_Mutex;
 };
 
 #endif
