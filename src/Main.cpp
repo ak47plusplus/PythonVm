0b6b34bef46fafc8954e3abaf5b85da126e57724
moveaxebc <mzygdeaq@163.com>
2019-11-01 11:28:49 +0800

    a

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <stdlib.h>
 #include "Logger.hpp"
 #include "FileInputStream.hpp"
 #include "BinaryFileParser.hpp"
@@ -14,8 +15,13 @@ int main(int argc, char const *argv[]) {
     // init the easylogging++
     START_EASYLOGGINGPP(argc, argv);
 
-    FileInputStream _stream("test.pyc");
-    auto parser = std::make_shared<BinaryFileParser>(&_stream);
-    parser->parse();
+    try {
+        FileInputStream _stream("test.pyc");
+        auto parser = std::make_shared<BinaryFileParser>(&_stream);
+        parser->parse();
+    } catch(std::exception &ex) {
+        std::cout << "Error: " << ex.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
