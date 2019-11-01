521d3777b7113cac7db5aead6b79829d1f8bc82e
root <root@localhost.localdomain>
2019-11-01 11:20:39 +0800

    s

---
+++ build/CMakeFiles/tinyPython.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/PythonVm/src/3rd/easylogging++/easylogging++.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/3rd/easylogging++/easylogging++.cpp.o"
+  "/opt/PythonVm/src/BinaryFileParser.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/BinaryFileParser.cpp.o"
+  "/opt/PythonVm/src/CodeObject.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/CodeObject.cpp.o"
+  "/opt/PythonVm/src/FileInputStream.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/FileInputStream.cpp.o"
+  "/opt/PythonVm/src/Interpreter.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/Interpreter.cpp.o"
+  "/opt/PythonVm/src/Main.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/Main.cpp.o"
+  "/opt/PythonVm/src/PyInteger.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/PyInteger.cpp.o"
+  "/opt/PythonVm/src/PyString.cpp" "/opt/PythonVm/build/CMakeFiles/tinyPython.dir/src/PyString.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/3rd/easylogging++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tinyPython.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyPython.dir/src/Main.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/3rd/easylogging++/easylogging++.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/BinaryFileParser.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/CodeObject.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/FileInputStream.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/Interpreter.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/PyInteger.cpp.o"
+  "CMakeFiles/tinyPython.dir/src/PyString.cpp.o"
+  "tinyPython.pdb"
+  "tinyPython"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyPython.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
