ad50f7a698f25a40a84415ab62373dade89ab7f1
mzygdeaq <mzygdeaq@163.com>
2019-11-03 09:49:58 +0800

    add vm.hpp

---
+++ src/VM.hpp
@@ -0,0 +1,21 @@
+#ifndef VM_HPP____
+#define VM_HPP____
+
+#include "Core.hpp"
+
+class PyInteger;
+class PyObject;
+
+class VM {
+public:
+    static void init() NOEXCEPT;
+    static void destroy() NOEXCEPT;
+private:
+
+public:
+    static PyInteger *PyTrue;
+    static PyInteger *PyFalse;
+    static PyObject   *PyNone;
+}
+
+#endif
