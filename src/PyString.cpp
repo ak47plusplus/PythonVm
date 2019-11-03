825a114e1ca0993c26914ece864040f7f4a12b94
mzygdeaq <mzygdeaq@163.com>
2019-11-03 17:18:58 +0800

    stringklass

--- src/PyString.cpp
+++ src/PyString.cpp
@@ -1,6 +1,76 @@
 #include "PyString.hpp"
-#include <cstring>
-#include <cstdlib>
+#include "VM.hpp"
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+StringKlass::StringKlass(){}
+
+StringKlass *StringKlass::m_Instance = nullptr;
+std::mutex StringKlass::m_Mutex;
+
+StringKlass *StringKlass::get_instance()
+{
+      if(StringKlass::m_Instance == nullptr)
+      {
+            std::lock_guard<std::mutex> lock(StringKlass::m_Mutex);
+            if(StringKlass::m_Instance == nullptr)
+            {
+                  StringKlass::m_Instance = new StringKlass();
+            }
+      }
+      return StringKlass::m_Instance;
+}
+
+// py的字符串里可以包含\0 请勿使用%s打印
+void StringKlass::print(PyObject *x)
+{
+    assert(x && x->klass() == static_cast<Klass*>(this));
+    PyString *pyStr = dynamic_cast<PyString*>(x);
+    for(int i = 0; i < pyStr->length(); i++)
+    {
+        printf("%c", pyStr->value()[i]);
+    }
+}
+
+PyObject* StringKlass::add(PyObject *lhs, PyObject *rhs)
+{
+    //  TODO
+    return nullptr;
+}
+
+
+PyObject* StringKlass::less(PyObject *lhs, PyObject *rhs)
+{
+    //  TODO
+    return nullptr;
+}
+
+PyObject* StringKlass::equal(PyObject *lhs, PyObject *rhs)
+{
+    if(lhs->klass() != rhs->klass()) return VM::PyFalse;
+    assert(lhs->klass() == static_cast<Klass*>(this));
+    assert(rhs->klass() == static_cast<Klass*>(this));
+
+    PyString *pLhs = dynamic_cast<PyString*>(lhs);
+    PyString *pRhs = dynamic_cast<PyString*>(rhs);
+
+    if(pLhs->length() != pRhs->length())
+    {
+        return VM::PyFalse;
+    }
+
+    for(int i = 0; i < pLhs->length(); ++i)
+    {
+        if(pLhs->value()[i] != pRhs->value()[i])
+            return VM:::PyFalse;
+    }
+    return VM::PyTrue;
+}
+
+// =================================== stringobject=============================
 
 // do not contain the '\0'
 PyString::PyString(const char * str)
@@ -26,6 +96,7 @@ PyString::PyString(const PyString& rhs)
     m_value = new char[m_length];
     memcpy(this->m_value,rhs.m_value, (size_t)m_length);
 }
+
 PyString& PyString::operator=(const PyString &rhs)
 {
   delete[] m_value;
@@ -38,6 +109,6 @@ PyString& PyString::operator=(const PyString &rhs)
 PyString::~PyString()
 {
     if (m_value != nullptr) {
-        delete[] m_value; 
+        delete[] m_value;
     }
 }
--- src/PyString.hpp
+++ src/PyString.hpp
@@ -1,22 +1,53 @@
 #ifndef PY_STRING_HPP
 #define PY_STRING_HPP
 
+#include <mutex>      // for std::mutex and std::lock_guard
+
+#include "Klass.hpp"
 #include "PyObject.hpp"
 
 /**
- *  @brief The Python String.
+ *  @brief The Python String class and object.
  *  @Created by GuoXiang 2019/10/29
  */
+
+class StringKlass : public Klass
+{
+public:
+    static StringKlass *get_instance();
+
+    virtual void print(PyObject *x);
+
+    virtual PyObject* add(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* sub(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* mul(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* div(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* mod(PyObject *lhs, PyObject *rhs);
+
+
+    // not supported: virtual PyObject* greater(PyObject *lhs, PyObject *rhs);
+    virtual PyObject* less(PyObject *lhs, PyObject *rhs);
+    virtual PyObject* equal(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* not_equal(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* ge(PyObject *lhs, PyObject *rhs);
+    // not supported: virtual PyObject* le(PyObject *lhs, PyObject *rhs);
+
+private:
+    StringKlass();
+    static StringKlass *m_Instance;
+    static std::mutex   m_Mutex;
+};
+
 class PyString : public PyObject {
 
 public:
   PyString(const char * str);
   PyString(const char * str, const int length);
   PyString(const PyString& rhs);
-  PyString& operator=(const PyString& rhs);
   ~PyString();
+  PyString& operator=(const PyString& rhs);
   const char *value() { return this->m_value;}
-  int length() {return this->m_length;}
+  int length()        {return this->m_length;}
 
 private:
     char * m_value;
