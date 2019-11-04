72050341b9489f6520993e922d47274c7b606a9e
moveaxebc <mzygdeaq@163.com>
2019-11-04 09:56:11 +0800

    aa

--- src/LoggerConfig.hpp
+++ src/LoggerConfig.hpp
@@ -1,10 +1,12 @@
 #ifndef LOGGER_CONFIG_HPP__
 #define LOGGER_CONFIG_HPP__
 
+#include <functional>
+
 class LoggerConfig {
 public:
     static void init();
-    static void register_format_specifier(char *fmtSp, (void *)(*func)(void));
+    static void register_format_specifier(char *fmtSp, std::function<std::string(const LogMessage*)>);
 };
 
 #endif
