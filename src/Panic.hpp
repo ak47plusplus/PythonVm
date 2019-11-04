fd2d2362af28c2589fc319a02e993ea14a582c16
moveaxebc <mzygdeaq@163.com>
2019-11-04 09:12:39 +0800

    format panic

--- src/Panic.hpp
+++ src/Panic.hpp
@@ -2,10 +2,11 @@
 #define PANIC_HPP__
 
 #include <stdio.h>
-#define __panic(format, ...)            \
-      do {                        \
-          fprintf(stderr,format,__VA_ARGS__);  \
-          abort();                \
+
+#define __panic(format, ...)                    \
+      do {                                      \
+          fprintf(stderr,format,__VA_ARGS__);   \
+          abort();                              \
       } while(0)
 
 #endif
