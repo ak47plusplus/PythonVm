21028342ac6332c29104a0716b689c4dc128d035
moveaxebc <mzygdeaq@163.com>
2019-11-06 14:58:37 +0800

    map finished

--- src/Map.hpp
+++ src/Map.hpp
@@ -1,6 +1,14 @@
 #ifndef MAP_HPP____
 #define MAP_HPP____
 
+#include "VM.h"
+
+#include <exception>
+#include <stdexcept>
+
+template<typename K, typename V>
+class Map<K,V>;
+
 template<typename K, typename V>
 class MapEntry {
 public:
@@ -8,6 +16,7 @@ public:
     MapEntry(K k, V v): m_K(k), m_V(v){}
     MapEntry(const MapEntry<K,V>& rhs);
     MapEntry& operator=(const MapEntry<K,V>& rhs);
+    friend class Map<K,V>;
 private:
     K m_K;
     V m_V;
@@ -27,12 +36,19 @@ MapEntry<K,V>& MapEntry<K,V>::operator=(const MapEntry<K,V>& rhs)
     this->m_V = rhs.m_V;
 }
 
+#define DEFAULT_MAP_INIT_CAP 8
+
 template<typename K, typename V>
 class Map {
 public:
     Map();
-    int     size()     const {return m_Size;}
-    int     capacity() const { return m_Capacity;}
+    Map(const Map<K,V>& rhs);
+    Map(Map<K,V>&& rhs);
+    Map<K,V>& operator=(const Map<K,V>& rhs);
+    Map<K,V>& operator=(Map<K,V>&& rhs);
+    ~Map();
+    int     size()     const {  return m_Size;}
+    int     capacity() const {  return m_Capacity;}
     void    put(K k, V v);
     V       get(K k);
     K       get_key(int index);
@@ -48,5 +64,157 @@ private:
     int            m_Size;
     int            m_Capacity;
 };
+
+template<typename K, typename V>
+Map<K,V>::Map()
+{
+    m_Entries   = new MapEntry<K,V>[DEFAULT_MAP_INIT_CAP];
+    m_Capacity  = DEFAULT_MAP_INIT_CAP;
+    m_Size      = 0;
+}
+
+template<typename K, typename V>
+Map<K,V>::Map(const Map<K,V>& rhs)
+{
+    m_Entries   = new MapEntry<K,V>[rhs.m_Capacity];
+    m_Capacity  = rhs.m_Capacity;
+    m_Size      = rhs.m_Size;
+    for (decltype(rhs.size()) i = 0; i < m_Size; i++)
+    {
+        m_Entries[i] = rhs.m_Entries[i];
+    }
+}
+
+template<typename K, typename V>
+Map<K,V>::Map(Map<K,V>&& rhs)
+{
+    m_Entries       = rhs.m_Entries;
+    m_Capacity      = rhs.m_Capacity;
+    m_Size          = rhs.m_Size;
+    rhs.m_Entries   = nullptr;
+    rhs.m_Capacity  = 0;
+    rhs.m_Size      = 0;
+}
+
+template<typename K, typename V>
+Map<K,V>& Map<K,V>::operator=(const Map<K,V>& rhs)
+{
+    if(m_Entries != nullptr)
+        delete m_Entries;
+    m_Entries   = new MapEntry<K,V>[rhs.m_Capacity];
+    m_Capacity  = rhs.m_Capacity;
+    m_Size      = rhs.m_Size;
+    for (decltype(rhs.size()) i = 0; i < m_Size; i++)
+    {
+        m_Entries[i] = rhs.m_Entries[i];
+    }
+    return *this;
+}
+
+template<typename K, typename V>
+Map<K,V>& Map<K,V>::operator=(Map<K,V>&& rhs)
+{
+    if(m_Entries != nullptr)
+        delete m_Entries;
+    m_Entries       = rhs.m_Entries;
+    m_Capacity      = rhs.m_Capacity;
+    m_Size          = rhs.m_Size;
+    rhs.m_Entries   = nullptr;
+    rhs.m_Capacity  = 0;
+    rhs.m_Size      = 0;
+    return *this;
+}
+
+template<typename K, typename V>
+Map<K,V>::~Map()
+{
+    if(m_Entries != nullptr)
+        delete []m_Entries;
+}
+
+template<typename K, typename V>
+void Map<K,V>::put(K k, V v)
+{
+    for(decltype(size()) i = 0; i < m_Size; i++)
+    {
+        if(m_Entries[i].m_K->equal(k) == VM::PyTrue)
+        {
+            m_Entries[i].m_V = v;
+            return;
+        }
+    }
+    if(m_Size >= m_Capacity)
+    {
+        this->expand_capacity();
+    }
+    m_Entries[m_Size++] = MapEntry<K,V>(k,v);
+}
+
+template<typename K, typename V>
+V Map<K,V>::get(K k)
+{
+    int idx = index(k);
+    if(idx < 0)
+        return VM::PyNone;
+    else
+        return m_Entries[idx].m_V;
+}
+
+template<typename K, typename V>
+K Map<K,V>::get_key(int idx)
+{
+    if(idx >= m_Size)
+        throw std::out_of_range("index out of bounds.");
+    return m_Entries[idx].m_K;
+}
+
+template<typename K, typename V>
+bool Map<K,V>::contains_key(K k)
+{
+    return has_key(k);
+}
+
+template<typename K, typename V>    // alias contains_key
+bool Map<K,V>::has_key(K k){
+    return index(k) >= 0;
+}
+
+template<typename K, typename V>
+V  Map<K,V>::remove(K k)
+{
+    int idx = index(k);
+    if(idx < 0)
+        return VM::PyNone;
+    V rtV = m_Entries[idx].m_V;
+    m_Entries[idx] = m_Entries[--m_Size];
+    return rtV;
+}
+
+template<typename K, typename V>
+int Map<K,V>::index(K k)
+{
+    for(decltype(size()) i = 0; i < m_Size; i++)
+    {
+        if(m_Entries[i].m_K->equal(k) == VM::PyTrue)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template<typename K, typename V>
+void expand_capacity()
+{
+    int newCap = m_Capacity << 1;
+    MapEntry<K,V> *newSpace = new MapEntry<K,V>[newCap];
+    for(decltype(size()) i = 0; i < m_Size; i++)
+    {
+        newSpace[i] = m_Entries[i];
+    }
+    delete m_Entries;
+    m_Entries = newSpace;
+    m_Capacity = newCap;
+}
 
 #endif
--- src/Stack.hpp
+++ src/Stack.hpp
@@ -5,8 +5,16 @@
 
 template<typename T>
 class Stack {
+typedef unsigned int        size_type;
+typedef T                   value_type;
+typedef value_type&         reference;
+typedef const value_type&   const_reference;
 public:
     Stack() = default;
+    size_type   size()                        { return m_InnerStack.size();}
+    void        push(const value_type& val)   { m_InnerStack.push(val);}
+    void        push(value_type&& val)        { m_InnerStack.push(val);}
+    reference&  top();
 private:
     std::stack<T> m_InnerStack;
 };
