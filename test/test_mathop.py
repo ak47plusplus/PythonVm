52fcd2361f710500b2bc9c56887c6d3218bb4ecc
root <root@localhost.localdomain>
2019-11-06 18:18:10 +0800

    虚拟机可以运行四则运算以及基本的if和while

---
+++ test/test_mathop.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+c = 3
+d = 4
+
+print "a + b", a+b
+print "a - b", a-b
+print "a * b", a*b
+print "a / b", a/b
+print "d % a", d%a
+
+a+=1
+b-=1
+c*=2
+d/=2
+print "a=",a
+print "b=",b
+print "c=",c
+print "d=",d
