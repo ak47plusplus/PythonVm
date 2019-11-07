4646b87747867a59b28e27b2b3859789681945c2
moveaxebc <mzygdeaq@163.com>
2019-11-07 18:38:02 +0800

    add

---
+++ src/PyFunction.hpp
@@ -0,0 +1,15 @@
+#ifndef PY_FUNCTION_HPP__
+#define PY_FUNCTION_HPP__
+
+#include "Core.hpp"
+#include "Klass.hpp"
+
+class FunctionKlass : public Klass {
+
+};
+
+class PyFunction : public PyObject {
+
+};
+
+#endif
