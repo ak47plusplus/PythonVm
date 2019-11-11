ac397717eec4c0dfb5b55ee8633c496bf28e592b
moveaxebc <mzygdeaq@163.com>
2019-11-11 18:33:06 +0800

    函数支持默认参数

--- src/PyFunction.hpp
+++ src/PyFunction.hpp
@@ -24,19 +24,26 @@ friend class Frame;
 friend class FunctionKlass;
 
 private:
-    CodeObject *m_FuncCode;
-    PyString   *m_FuncName;
-    uint32_t    m_Flags;
+    CodeObject             *m_FuncCode  __PTR_IN__;
+    PyString               *m_FuncName  __PTR_IN__;
+    uint32_t                m_Flags;
 
-    Map<PyObject*, PyObject*> *m_Globals;
+    /// 函数的默认参数
+    ArrayList<PyObject*>   *m_DefaultArgs __PTR_INNER__;
+
+    /// 函数所依赖的全局变量表是定义函数对象的时候的，而不是调用函数时候的 这个字段纯属无奈之举
+    Map<PyObject*, PyObject*> *m_Globals __PTR_IN__; 
 public:
     PyFunction(CodeObject *codeObj);
     PyFunction(Klass *klass);
+    ~PyFunction();
     PyString*                   func_name()   { return m_FuncName;}
     uint32_t                    flags()       { return m_Flags;}
     Map<PyObject*, PyObject*>  *globals()     { return m_Globals;}
     void                        set_globals(Map<PyObject*, PyObject*>  *_globals)
                                               { m_Globals = _globals;}
+    ArrayList<PyObject*>       *default_args(){ return m_DefaultArgs;}
+    void                        set_default_args(ArrayList<PyObject*> *_default_args);
 };
 
 #endif
