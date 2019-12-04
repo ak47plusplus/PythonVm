38de11556aa8bbe64a39de7fe5d914359895c92a
moveaxebc <mzygdeaq@163.com>
2019-12-04 16:14:59 +0800

    支持pydict的内建方法

--- src/object/PyDict.cpp
+++ src/object/PyDict.cpp
@@ -18,11 +18,12 @@ DictKlass::DictKlass()
 }
 
 void DictKlass::InitKlass()
-{
+{   
+    /* 初始化dict的内置方法  */
     PyDict *attrs = new PyDict(8);
-    attrs->put(new PyString("clear"),       new PyFunction(pydict::dict_clear));
-    attrs->put(new PyString("dict_keys"),   new PyFunction(pydict::dict_keys));
-    attrs->put(new PyString("dict_values"), new PyFunction(pydict::dict_values));
+    attrs->put(new PyString("clear"),  new PyFunction(pydict::dict_clear));
+    attrs->put(new PyString("keys"),   new PyFunction(pydict::dict_keys));
+    attrs->put(new PyString("values"), new PyFunction(pydict::dict_values));
     set_attrs(attrs);
 }
 
